cdda35f8e4e747c8300af647d409660214cc2b72
francesca@datachemist.com <francesca@datachemist.com>
2019-09-16 15:15:26 +0100

    fix post call

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ var TerminusDB = require('./index.js');
 
 var dbClient= new TerminusDB.WOQLClient();
 
-var connection=dbClient.connect("https://dev.datachemist.net/platform", "root");
 
-connection.then((response)=>console.log("I'm connect",response)).catch((err)=>{
+
+var connection=dbClient.connect("http://195.201.12.87:6363", "root");
+
+connection.then((response)=>{
+	console.log("I'm connect")
+
+
+	dbClient.getSchema(getSchema,{"terminus:encoding":"terminus:turtle"}).then((response)=>
+	console.log("getSchema RESPONSE OK")).catch((err)=>{
+	console.log("PROMISE reject", 'GETSCHEMA');
+});
+	
+
+}).catch((err)=>{
 	console.log("PROMISE reject", 'CONNECTION');
 });
 
-dbClient.getClassFrame('http://pippo').then(response=>{console.log(response)}).catch(err=>{
-	console.log("GETFRAME ERROR");
-})
+//dbClient.getClassFrame('http://pippo').then(response=>{console.log(response)}).catch(err=>{
+//	console.log("GETFRAME ERROR");
+//})
 
-var cc= new TerminusDB.IDParser();
+var getSchema='terminus';
+
+
+
+
+
+//var cc= new TerminusDB.IDParser();
 
 //https://github.com/caolan/async
