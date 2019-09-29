ea6d780b80773b4badc7821083221093625824d8
francesca@datachemist.com <francesca@datachemist.com>
2019-09-29 10:54:49 +0100

    fix btoa in node

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var TerminusClient = require('./index.js');
 
 var dbClient= new TerminusClient.WOQLClient();
 
-var connection=dbClient.connect("http://localhost");
+var connection=dbClient.connect("http://localhost:6363/",'root');
 
 connection.then((response)=>{
-	console.log("I'm connect")
+	console.log("I'm connect",response)
 	dbClient.getSchema(getSchema,{"terminus:encoding":"terminus:turtle"}).then((response)=>
+
 	console.log("getSchema RESPONSE OK")).catch((err)=>{
 	console.log("PROMISE reject", 'GETSCHEMA');
 });
