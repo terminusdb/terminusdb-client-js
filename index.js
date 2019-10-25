ff0b9d0b4ac395bf76b63c56cee8f35371dbaf7d
kevin@datachemist.com <kevin@datachemist.com>
2019-10-25 13:55:52 +0100

    updated new client code to make it work with packaging system

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
-module.exports = {WOQLClient:require('./lib/woqlClient'),
+module.exports = {
+				 WOQLClient:require('./lib/woqlClient'),
                   ConnectionCapabilities:require('./lib/connectionCapabilities'),
                   ConnectionConfig:require('./lib/connectionConfig'),
                   IDParser:require('./lib/terminusIDParser'),
                   ErrorMessage:require('./lib/errorMessage'),
                   FrameHelper:require('./lib/frameHelper'),
-                  ObjectFrame:require('./lib/objectFrame')};
+                  ObjectFrame:require('./lib/objectFrame'),
+                  TerminusDocumentViewer:require('./lib/documentViewer'),
+                  WOQLViewer:require('./lib/woqlViewer'),
+                  WOQL:require('./lib/woql')};
