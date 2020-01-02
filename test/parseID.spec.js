be0edb5bb96cd1f5bbdfb98e0577359c68e73b47
Kevin Feeney <kevin@datachemist.com>
2020-01-02 17:15:45 +0000

    update to terminus client libs

--- test/parseID.spec.js
+++ test/parseID.spec.js
@@ -81,32 +81,29 @@ describe('IDParser tests', function () {
 		expect(idParser.dbid()).to.equal(dbName)
 	})
 
-	/*
-	I have to ask the frame of a database to the TERMINUSDB
-	the call is for the terminusdb 
-	*/
-
-	/*it('test parse parseClassFrameURL',function(){	
+	it('test parse parseClassFrameURL',function(){	
 		const woqlclientFullURL="http://localhost:6363/terminus/frame"
+    	const servURL="http://localhost:6363/"
     	const dbName="terminus"
-		expect(idParser.parseQueryURL(woqlclientFullURL)).to.equal(dbName)
+		expect(idParser.parseClassFrameURL(woqlclientFullURL)).to.equal(dbName)
 		expect(idParser.server()).to.equal(servURL)
 		expect(idParser.dbid()).to.equal(dbName)
-	})*/
+	})
 
 
 	it('test validPrefixedURL',function(){
 		const context= {
-				    "doc":"http://195.201.12.87:6363/terminus/document/",
-				    "owl":"http://www.w3.org/2002/07/owl#",
-				    "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-				    "rdfs":"http://www.w3.org/2000/01/rdf-schema#",
-				    "xsd":"http://www.w3.org/2001/XMLSchema#"
-				  }
+		    "doc":"http://localhost:6363/terminus/document/",
+		    "owl":"http://www.w3.org/2002/07/owl#",
+		    "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+		    "rdfs":"http://www.w3.org/2000/01/rdf-schema#",
+		    "xsd":"http://www.w3.org/2001/XMLSchema#"
+		}
         
-        const docFullUrl='http://195.201.12.87:6363/terminus/document/myDocument'
+        const docFullUrl='http://localhost:6363/terminus/document/myDocument'
 		idParser.setContext(context);
 		expect(idParser.validPrefixedURL('doc:myDocument',context)).to.equal(true);
+		expect(idParser.validPrefixedURL('dic:myDocument',context)).to.equal(false);
 		expect(idParser.expandPrefixed('doc:myDocument',context)).to.equal(docFullUrl);
 	})
 })
