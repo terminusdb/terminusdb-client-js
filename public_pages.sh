84bf8da044df0bc41e72cf084fda93c0c487deab
francesca@datachemist.com <francesca@datachemist.com>
2020-02-02 17:20:51 +0000

    test [run deploy] 1.1.4 release

--- public_pages.sh
+++ public_pages.sh
@@ -12,6 +12,8 @@ pushd "$HOME" || exit
 git clone --branch=$PUBLICATION_BRANCH  "https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG" tmp_pages 2>&1 > /dev/null
 cd tmp_pages || exit
 
+rm -rf 1.1.14
+rm -rf 1.1.13
 # Update pages
 cp -r $REPO_PATH/public_pages/.  .
 # Commit and push latest version
