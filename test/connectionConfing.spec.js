bd603893c8c77bec10fec4fd25f563da23bb4eb9
francesca@datachemist.com <francesca@datachemist.com>
2020-04-28 16:34:52 +0100

    review connectionConfig tests

--- test/connectionConfing.spec.js
+++ test/connectionConfing.spec.js
@@ -6,20 +6,35 @@ describe('connectionConfig tests', function () {
    
    const startServerUrl="http://localhost:6363/";
    const startDBid="testDB";
-   
-   const params={server:startServerUrl,db:startDBid}
+   const account="admin"
+   const params={server:startServerUrl,db:startDBid,account:account}
    const connectionConfig= new ConnectionConfig(params);
 
-   const newServerUrl="https://myTest.net/";
-   
+   const dbURL="http://localhost:6363/db/admin/testDB";
+
    it('check get server URL', function(){
       expect(connectionConfig.serverURL()).to.equal(startServerUrl);
-      expect(connectionConfig.dbURL()).to.equal(`${startServerUrl}${startDBid}`); 		
+      expect(connectionConfig.dbURL()).to.equal(dbURL); 		
    })
 
    it('change server', function(){
-      connectionConfig.setServer(newServerUrl);
-      expect(connectionConfig.serverURL()).to.not.equal(startServerUrl);
-      expect(connectionConfig.serverURL()).to.equal(newServerUrl);
+      connectionConfig.setBranch("myBranch");
+      /*
+      * the dbURL 
+      */
+      expect(connectionConfig.dbURL()).to.equal(dbURL);
+      //console.log(JSON.stringify(connectionConfig.queryURL(), null, 4));
+      
+      //'http://localhost:6363/db/admin/testDB
+      expect(connectionConfig.queryURL()).to.equal("http://localhost:6363/woql/admin/testDB/local/branch/myBranch");
+   })
+
+   it('change server', function(){
+      connectionConfig.setRef("gfhfjkflfgorpyuiioo");
+
+      //console.log(JSON.stringify(connectionConfig.queryURL(), null, 4));
+      
+      //'http://localhost:6363/db/admin/testDB
+      expect(connectionConfig.queryURL()).to.equal("http://localhost:6363/woql/admin/testDB/local/commit/gfhfjkflfgorpyuiioo");
    })
 })
