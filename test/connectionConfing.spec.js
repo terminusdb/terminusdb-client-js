92e0d9bbc139b47e69c837b04d6e7dadb32617bd
francesca@datachemist.com <francesca@datachemist.com>
2020-04-28 17:17:25 +0100

    fixed parseId tests

--- test/connectionConfing.spec.js
+++ test/connectionConfing.spec.js
@@ -17,24 +17,68 @@ describe('connectionConfig tests', function () {
       expect(connectionConfig.dbURL()).to.equal(dbURL); 		
    })
 
-   it('change server', function(){
+   it('check set branch', function(){
       connectionConfig.setBranch("myBranch");
+      const queryURLBranch="http://localhost:6363/woql/admin/testDB/local/branch/myBranch";
       /*
-      * the dbURL 
+      * the dbURL dosen't change
       */
       expect(connectionConfig.dbURL()).to.equal(dbURL);
-      //console.log(JSON.stringify(connectionConfig.queryURL(), null, 4));
-      
-      //'http://localhost:6363/db/admin/testDB
-      expect(connectionConfig.queryURL()).to.equal("http://localhost:6363/woql/admin/testDB/local/branch/myBranch");
+      expect(connectionConfig.queryURL()).to.equal(queryURLBranch);
    })
 
-   it('change server', function(){
+   it('check set refId', function(){
       connectionConfig.setRef("gfhfjkflfgorpyuiioo");
 
-      //console.log(JSON.stringify(connectionConfig.queryURL(), null, 4));
+      const queryURL="http://localhost:6363/woql/admin/testDB/local/commit/gfhfjkflfgorpyuiioo"
+
+      expect(connectionConfig.queryURL()).to.equal(queryURL);
+   })
+
+   it('check set class frameUrl', function(){
+      const classFrameURL="http://localhost:6363/frame/admin/testDB/local/commit/gfhfjkflfgorpyuiioo"
+
+      //console.log(JSON.stringify(connectionConfig.triplesURL(), null, 4));
       
-      //'http://localhost:6363/db/admin/testDB
-      expect(connectionConfig.queryURL()).to.equal("http://localhost:6363/woql/admin/testDB/local/commit/gfhfjkflfgorpyuiioo");
+      expect(connectionConfig.classFrameURL()).to.equal(classFrameURL);
+   })
+
+
+   it('check set class tripleUrl', function(){
+      const classTripleURL="http://localhost:6363/triples/admin/testDB/local/commit/gfhfjkflfgorpyuiioo"
+
+     // console.log(JSON.stringify(connectionConfig.triplesURL(), null, 4));
+      
+      expect(connectionConfig.triplesURL()).to.equal(classTripleURL);
+   })
+
+   it('check remove the refCommit', function(){
+      const queryUrlBranch01="http://localhost:6363/woql/admin/testDB/local/branch/myBranch";
+      const queryFrameBranch01="http://localhost:6363/frame/admin/testDB/local/branch/myBranch";
+      const queryTriplesBranch01="http://localhost:6363/triples/admin/testDB/local/branch/myBranch"; 
+      /*
+      *remove the ref commit it come to the 
+      */
+      connectionConfig.setRef(false);
+      expect(connectionConfig.queryURL()).to.equal(queryUrlBranch01);
+      expect(connectionConfig.classFrameURL()).to.equal(queryFrameBranch01);
+      expect(connectionConfig.triplesURL()).to.equal(queryTriplesBranch01);
+
+      //console.log(JSON.stringify(connectionConfig.queryURL(), null, 4));
+   })
+
+   it('check remove the branch', function(){
+      const queryUrlBranch01="http://localhost:6363/woql/admin/testDB/local/branch/master";
+      const queryFrameBranch01="http://localhost:6363/frame/admin/testDB/local/branch/master";
+      const queryTriplesBranch01="http://localhost:6363/triples/admin/testDB/local/branch/master"; 
+      /*
+      *remove the ref commit it come to the 
+      */
+      connectionConfig.setBranch(false);
+      expect(connectionConfig.queryURL()).to.equal(queryUrlBranch01);
+      expect(connectionConfig.classFrameURL()).to.equal(queryFrameBranch01);
+      expect(connectionConfig.triplesURL()).to.equal(queryTriplesBranch01);
+
+      //console.log(JSON.stringify(connectionConfig.queryURL(), null, 4));
    })
 })
--- test/parseID.spec.js
+++ test/parseID.spec.js
@@ -10,102 +10,29 @@ describe('IDParser tests', function () {
 		idParser=new IDParser();
 	})	
 
-	it('check default values',function(){		
-		expect(idParser.server()).to.equal(false);
-		expect(idParser.dbid()).to.equal(false);
-		expect(idParser.docid()).to.equal(false);	
-	})
 
 	it('test parse serverURL',function(){
 		expect(idParser.validURL(servURL)).to.equal(true);
 		expect(idParser.parseServerURL(servURL)).to.equal(servURL);
-		expect(idParser.server()).to.equal(servURL);
 	})
 
 	it('test parse parseDBID',function(){
 		const dbName="terminus"
 		//check db as TerminusDB name
 		expect(idParser.parseDBID(dbName)).to.equal(dbName);
-		expect(idParser.dbid()).to.equal(dbName);
-		expect(idParser.server()).to.equal(false);
-
-		//check db name as TerminusDB Url
-		expect(idParser.parseDBID(servURL+dbName)).to.equal(dbName)
-		expect(idParser.dbid()).to.equal(dbName);
-		expect(idParser.server()).to.equal(servURL);
 	})
 
-	//@param {string} docURL Terminus Document URL or Terminus Document ID
-	it('test parse parseDocumentURL',function(){
-		const docURL="http://localhost:6363/masterdb/document/admin"
-		const dbName="masterdb"
-		const documentName="admin"
-
-		expect(idParser.parseDocumentURL(docURL)).to.equal(documentName)
-		expect(idParser.server()).to.equal(servURL)
-		expect(idParser.dbid()).to.equal(dbName)
-		expect(idParser.docid()).to.equal(documentName)
-
-		const newDocumentName='newDocument'
-		//set a document as documentName this didn't change serverURL and dbID name
-		expect(idParser.parseDocumentURL(newDocumentName)).to.equal(newDocumentName)
-		expect(idParser.server()).to.equal(servURL)
-		expect(idParser.dbid()).to.equal(dbName)
-		expect(idParser.docid()).to.equal(newDocumentName)
+	it('test parse parseAccount',function(){
+		const account="admin"
+		//check db as TerminusDB name
+		expect(idParser.parseAccount(account)).to.equal(account);
 	})
 
-	//check if the url of get schema is corrects
-	it('test parse parseSchemaURL',function(){	
-		const schemaFullUrl="http://localhost:6363/masterdb/schema"
-		const dbName="masterdb"
-		expect(idParser.parseSchemaURL(schemaFullUrl)).to.equal(dbName)
-		expect(idParser.server()).to.equal(servURL)
-		expect(idParser.dbid()).to.equal(dbName)
-	})
-
-	//check if the url of get schema is corrects
-	it('test parse parseQueryURL',function(){	
-		const woqlclientFullURL="http://localhost:6363/terminus/woql"
-    	const dbName="terminus"
-		expect(idParser.parseQueryURL(woqlclientFullURL)).to.equal(dbName)
-		expect(idParser.server()).to.equal(servURL)
-		expect(idParser.dbid()).to.equal(dbName)
-	})
-
-	//check if the url of get schema is corrects
-	it('test parse parseQueryURL',function(){	
-		const woqlclientFullURL="http://localhost:6363/terminus/woql"
-    	const dbName="terminus"
-		expect(idParser.parseQueryURL(woqlclientFullURL)).to.equal(dbName)
-		expect(idParser.server()).to.equal(servURL)
-		expect(idParser.dbid()).to.equal(dbName)
-	})
-
-	it('test parse parseClassFrameURL',function(){	
-		const woqlclientFullURL="http://localhost:6363/terminus/frame"
-    	const servURL="http://localhost:6363/"
-    	const dbName="terminus"
-		expect(idParser.parseClassFrameURL(woqlclientFullURL)).to.equal(dbName)
-		expect(idParser.server()).to.equal(servURL)
-		expect(idParser.dbid()).to.equal(dbName)
-	})
-
-
-	it('test validPrefixedURL',function(){
-		const context= {
-		    "doc":"http://localhost:6363/terminus/document/",
-		    "owl":"http://www.w3.org/2002/07/owl#",
-		    "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-		    "rdfs":"http://www.w3.org/2000/01/rdf-schema#",
-		    "xsd":"http://www.w3.org/2001/XMLSchema#"
-		}
-        
-        const docFullUrl='http://localhost:6363/terminus/document/myDocument'
-		idParser.setContext(context);
-		expect(idParser.validPrefixedURL('doc:myDocument',context)).to.equal(true);
-		expect(idParser.validPrefixedURL('dic:myDocument',context)).to.equal(false);
-		expect(idParser.expandPrefixed('doc:myDocument',context)).to.equal(docFullUrl);
-	})
+	it('test parse parseBranch',function(){
+		const branch="myBranch"
+		//check db as TerminusDB name
+		expect(idParser.parseBranch(branch)).to.equal(branch);
+	})	
 })
 
 
