98cae6765c8b004e753c84e31dedd70d9ce00d4f
Robin de Rooij <rderooij685@gmail.com>
2020-05-12 14:48:24 +0200

    ci: actually make the version variable
    Whoops

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -5,5 +5,6 @@ always-auth=true
 registry=https://packagecloud.io/rrooij/development/npm/
 //packagecloud.io/rrooij/development/npm/:_authToken=$PACKAGECLOUD_API_TOKEN
 " > $TRAVIS_BUILD_DIR/.npmrc
+VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 npm unpublish "@terminusdb/terminusdb-client@$VERSION" || true
 npm publish
