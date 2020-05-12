52fc104a48a79288ecbfc93f2269a5c351e5452a
Robin de Rooij <rderooij685@gmail.com>
2020-05-12 10:40:12 +0200

    ci: forgot to add script

---
+++ .github/workflows/publish_github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+npm config set registry https://npm.pkg.github.com/
+VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
+npm unpublish "@terminusdb/terminusdb-client@$VERSION"
+npm publish
