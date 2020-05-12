04be5914804d61e0248c6a57fa0f067af937b9ed
Robin de Rooij <rderooij685@gmail.com>
2020-05-12 13:47:04 +0200

    ci: create npmrc first

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm ci
-npm set registry https://packagecloud.io/rrooij/development/npm/
 echo "//packagecloud.io/rrooij/development/npm/:_authToken=$PACKAGECLOUD_TOKEN" > $HOME/.npmrc
+npm set registry https://packagecloud.io/rrooij/development/npm/
 npm publish
