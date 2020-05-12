56bb308566afbda00687a6f7f3134f88a4ff6f35
Robin de Rooij <rderooij685@gmail.com>
2020-05-12 11:15:19 +0200

    ci: double quotes again
    When will this stop?

--- .github/workflows/publish_github.sh
+++ .github/workflows/publish_github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
-npm unpublish @terminusdb/terminusdb-client@$VERSION
+npm unpublish "@terminusdb/terminusdb-client@$VERSION"
 npm publish
