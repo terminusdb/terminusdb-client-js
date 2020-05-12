a14f1e235fb11e9966feeba07e84869bf409ed90
Robin de Rooij <rderooij685@gmail.com>
2020-05-12 14:24:01 +0200

    ci: fix emberassing typo

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -3,6 +3,6 @@ npm ci
 echo "\
 always-auth=true
 registry=https://packagecloud.io/rrooij/development/npm/
-//packagecloud.io/rrooij/development/npm/:_authToken=$PACKAGECLOUD_TOKEN
+//packagecloud.io/rrooij/development/npm/:_authToken=$PACKAGECLOUD_API_TOKEN
 " > $TRAVIS_BUILD_DIR/.npmrc
 npm publish
