e736650d231493afe6a5902d625e32be2b8b7b9b
Robin de Rooij <rderooij685@gmail.com>
2020-05-12 11:08:36 +0200

    ci: set npm registry url

--- .github/workflows/publish_github.sh
+++ .github/workflows/publish_github.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-npm config set registry https://npm.pkg.github.com/
 VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 npm unpublish @terminusdb/terminusdb-client@$VERSION
 npm publish
