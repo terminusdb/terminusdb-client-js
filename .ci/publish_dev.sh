c16f36ffa90b1501d3e1e8ac6bd4f798ec3cc5a8
Robin de Rooij <rderooij685@gmail.com>
2020-05-13 16:18:40 +0200

    ci: fix configuration

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -2,9 +2,9 @@
 npm ci
 echo "\
 registry=https://api.bintray.com/npm/terminusdb/npm-dev
-_auth=rrooij:$BINTRAY_TOKEN
-email=robin@datachemist.com
-always-auth=true" > $TRAVIS_BUILD_DIR/.npmrc
+_auth=$BINTRAY_TOKEN
+always-auth=true
+email=robin@datachemist.com" > $TRAVIS_BUILD_DIR/.npmrc
 VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 npm unpublish "@terminusdb/terminusdb-client@$VERSION"
 npm publish
