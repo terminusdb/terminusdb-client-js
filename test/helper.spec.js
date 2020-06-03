c07e60cce1afc9621ff6914228f88a7faac3b71e
francesca@datachemist.com <francesca@datachemist.com>
2020-06-03 10:37:02 +0100

    review tests

--- test/helper.spec.js
+++ test/helper.spec.js
@@ -1,37 +1,40 @@
 'use strict'
 //const axios = require("axios");
-const axiosInstance = require('../lib/axiosInstance');
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const WOQLClient = require('../lib/woqlClient'); 
-const CONNECT_RESPONSE = require('./serverResponse/connectResponseForCapabilities');
+const axiosInstance = require('../lib/axiosInstance')
+const expect = require('chai').expect
+const sinon = require('sinon')
+const WOQLClient = require('../lib/woqlClient')
+const CONNECT_RESPONSE = require('./serverResponse/connectResponseForCapabilities')
 
 before(function(done) {
-	console.log('before all test');
-	global.url  = 'http://localhost:6363/';
-	const key='root';
-	global.sandbox = sinon.createSandbox();
-	global.sandbox.stub(axiosInstance, "get").returns(Promise.resolve({status:200, data: CONNECT_RESPONSE}));
-	
-	global.client=new WOQLClient();			
-  	global.client.connect({server:global.url,key:key}).then(function(response){
-	/*
-	*check that the connection object is filled well
-	*/
+  console.log('before all test')
+  global.url = 'http://localhost:6363/'
+  const key = 'root'
+  global.sandbox = sinon.createSandbox()
+  global.sandbox
+    .stub(axiosInstance, 'get')
+    .returns(Promise.resolve({status: 200, data: CONNECT_RESPONSE}))
 
-	global.sandbox.restore();
+  global.client = new WOQLClient(global.url)
+  global.client
+    .connect({key: key, user: 'admin', account: 'admin'})
+    .then(function(response) {
+      /*
+       *check that the connection object is filled well
+       */
 
-	}).then(done, done);
+      global.sandbox.restore()
+    })
+    .then(done, done)
 })
 
 beforeEach(function() {
-   global.sandbox = sinon.createSandbox();
+  global.sandbox = sinon.createSandbox()
+})
 
-});
-
-afterEach(function(){
-	//server.restore();
-    global.sandbox.restore();
-});
+afterEach(function() {
+  //server.restore();
+  global.sandbox.restore()
+})
 
 //module.exports={client:client,sandbox:sandbox}
