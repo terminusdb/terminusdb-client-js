0af78155ed5a360141549ffad53668d97296d6ee
Kevin Feeney <kevin@datachemist.com>
2020-06-25 13:02:47 +0100

    added release notes for client v2.0.4

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var TerminusClient = require('./index.js');
+var TerminusClient = require('./index.js')
 
-var dbClient= new TerminusClient.WOQLClient();
+var dbClient = new TerminusClient.WOQLClient()
 
-var connection=dbClient.connect("http://localhost:6363/",'root');
+var connection = dbClient.connect('http://localhost:6363/', 'root')
 
-connection.then((response)=>{
-	console.log("I'm connect",response)
-	dbClient.getSchema(getSchema,{"terminus:encoding":"terminus:turtle"}).then((response)=>
-
-	console.log("getSchema RESPONSE OK")).catch((err)=>{
-	console.log("PROMISE reject", 'GETSCHEMA');
-});
-
-}).catch((err)=>{
-	console.log("PROMISE reject", 'CONNECTION');
-});
-
+connection
+    .then(response => {
+        console.log("I'm connect", response)
+        dbClient
+            .getSchema(getSchema, {'terminus:encoding': 'terminus:turtle'})
+            .then(response => console.log('getSchema RESPONSE OK'))
+            .catch(err => {
+                console.log('PROMISE reject', 'GETSCHEMA')
+            })
+    })
+    .catch(err => {
+        console.log('PROMISE reject', 'CONNECTION')
+    })
