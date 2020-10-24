1f4f2728a16750000390e531d0ca8d6153d8a72c
Robin de Rooij <rderooij685@gmail.com>
2020-10-24 22:46:27 +0200

    ci: use proper authentication

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -urrooij:$BINTRAY_CURL_API_KEY https://api.bintray.com/npm/terminusdb/npm-dev/auth > .npmrc
+curl -u"rrooij:$BINTRAY_CURL_API_KEY" https://api.bintray.com/npm/terminusdb/npm-dev/auth > .npmrc
 curl -XDELETE https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb-client -u "rrooij:$BINTRAY_API_TOKEN"
 npm publish --access=public
 curl -T "dist/terminusdb-client.min.js" -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js?publish=1&override=1"
