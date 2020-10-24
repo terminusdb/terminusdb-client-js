bbf5a4c517768855c1112ca3ba1454451ef256ac
Robin de Rooij <rderooij685@gmail.com>
2020-10-24 22:23:47 +0200

    ci: get auth information with curl

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-echo "\
-registry=https://api.bintray.com/npm/terminusdb/npm-dev
-_auth=$BINTRAY_TOKEN
-always-auth=true
-email=robin@datachemist.com" > $TRAVIS_BUILD_DIR/.npmrc
-curl -XDELETE https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb:terminusdb-client -u "rrooij:$BINTRAY_API_TOKEN"
+curl -urrooij:$BINTRAY_API_TOKEN https://api.bintray.com/npm/terminusdb/npm-dev/auth > $TRAVIS_BUILD_DIR/.npmrc
+curl -XDELETE https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb-client -u "rrooij:$BINTRAY_API_TOKEN"
 npm publish --access=public
 curl -T "dist/terminusdb-client.min.js" -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js?publish=1&override=1"
 curl -T "dist/terminusdb-client.min.js.map" -u"rrooij:$BINTRAY_CURL_API_KEY" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js.map?publish=1&override=1"
