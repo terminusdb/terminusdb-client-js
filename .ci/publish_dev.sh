e7df620511c646e7d9d9dde4a96b10d61b7bd4e8
Robin de Rooij <rderooij685@gmail.com>
2020-10-29 15:18:55 +0100

    Revert "ci: run npm install after bintray setup"
    This reverts commit d07458cf9278646d49b16d6d464ac6c277da9ca5.

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -u "rrooij:$BINTRAY_API_TOKEN" https://api.bintray.com/npm/terminusdb/npm-dev/auth > .npmrc
 curl -XDELETE https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb:terminusdb-client -u "rrooij:$BINTRAY_API_TOKEN"
 npm publish
 curl -T "dist/terminusdb-client.min.js" -u"rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js?publish=1&override=1"
