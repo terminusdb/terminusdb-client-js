d07458cf9278646d49b16d6d464ac6c277da9ca5
Robin de Rooij <rderooij685@gmail.com>
2020-10-29 15:10:02 +0100

    ci: run npm install after bintray setup

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -u "rrooij:$BINTRAY_API_TOKEN" https://api.bintray.com/npm/terminusdb/npm-dev/auth > .npmrc
 curl -XDELETE https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb:terminusdb-client -u "rrooij:$BINTRAY_API_TOKEN"
 npm publish --access=public
 curl -T "dist/terminusdb-client.min.js" -u"rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js?publish=1&override=1"
