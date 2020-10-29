8da38ceb60d94174ce75a057f70a57676ce72fa0
Robin de Rooij <rderooij685@gmail.com>
2020-10-29 15:14:25 +0100

    ci: remove access public option

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -XDELETE https://api.bintray.com/packages/terminusdb/npm-dev/terminusdb:terminusdb-client -u "rrooij:$BINTRAY_API_TOKEN"
-npm publish --access=public
+npm publish
 curl -T "dist/terminusdb-client.min.js" -u"rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js?publish=1&override=1"
 curl -T "dist/terminusdb-client.min.js.map" -u"rrooij:$BINTRAY_API_TOKEN" "https://api.bintray.com/content/terminusdb/terminusdb/terminusdb-client/dev/dev/terminusdb-client.min.js.map?publish=1&override=1"
