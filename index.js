f74251193ba378bc881f3dd04ffb918a8e057250
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-01-17 07:58:24 +0100

    Annotations helping typescript generator to generate typings

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
 module.exports = {
     WOQLClient:require('./lib/woqlClient'),
+    /**
+     * @type {typeof import('./lib/utils')}
+     */
     UTILS:require('./lib/utils'),
+    /**
+     * @type {typeof import('./lib/viewer/woqlView')}
+     */
     View:require('./lib/viewer/woqlView'),
+    /**
+     * @type {typeof import('./lib/woql')}
+     */
     WOQL:require('./lib/woql'),
     WOQLResult:require('./lib/viewer/woqlResult'),
     WOQLTable:require('./lib/viewer/woqlTable'),
