ab8b671c323a13de52334a3000e64fb436418b04
francesca@datachemist.com <francesca@datachemist.com>
2021-02-11 11:10:45 +0000

    review import

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 module.exports = {
     /**
-     * @type {typeof import('./lib/woqlClient').WOQLClient}
+     * @type {typeof import('./lib/woqlClient')}
      */
     WOQLClient: require('./lib/woqlClient'),
     /**
-     * @type {typeof import('./lib/utils').UTILS}
+     * @type {import('./lib/utils')}
      */
     UTILS: require('./lib/utils'),
     /**
-     * @type {typeof import('./lib/woqlView').View}
+     * @type {import('./lib/woqlView')}
      */
     View: require('./lib/viewer/woqlView'),
     /**
-     * @type {typeof import('./lib/woql').WOQL}
+     * @type {import('./lib/woql')}
      */
     WOQL: require('./lib/woql'),
     /**
-     * @type {typeof import('./lib/viewer/woqlResult').WOQLResult}
+     * @type {import('./lib/viewer/woqlResult')}
      */
     WOQLResult: require('./lib/viewer/woqlResult'),
     /**
-     * @type {typeof import('./lib/viewer/woqlTable').WOQLTable}
+     * @type {typeof import('./lib/viewer/woqlTable')}
      */
     WOQLTable: require('./lib/viewer/woqlTable'),
     /**
-     * @type {typeof import('./lib/viewer/woqlGraph').WOQLGraph}
+     * @type {typeof import('./lib/viewer/woqlGraph')}
      */
     WOQLGraph: require('./lib/viewer/woqlGraph'),
     /**
-     * @type {typeof import('./lib/axiosInstance').axiosInstance}
+     * @type {typeof import('./lib/axiosInstance')}
      */
     axiosInstance: require('./lib/axiosInstance'),
 }
