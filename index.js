9a6bb35250531731752398301ee6ea450b7cfb24
francesca@datachemist.com <francesca@datachemist.com>
2021-08-18 10:05:21 +0100

    change woqlClient

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ module.exports = {
      */
     WOQLClient: require('./lib/woqlClient'),
     /**
-     * @type {typeof import('./lib/woqlClientTools')}
-     */
-    WOQLClientTools: require('./lib/woqlClientTools'),
-    /**
      * @type {typeof import('./lib/utils')}
      */
     UTILS: require('./lib/utils'),
