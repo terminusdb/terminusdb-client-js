1ee1d39c4baa0427596f5a93f1e3ca6d7918ec39
Francesca-Bit <francesca@terminusdb.com>
2022-10-04 12:49:49 +0100

    fix type declaration

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /* eslint-disable global-require */
-const Vars = require('./lib/query/woqlDoc')
-const WOQLClient= require('./lib/woqlClient')
-const UTILS = require('./lib/utils')
-const View =require('./lib/viewer/woqlView')
-const WOQL =  require('./lib/woql')
-const WOQLResult= require('./lib/viewer/woqlResult')
-const WOQLTable= require('./lib/viewer/woqlTable')
-const WOQLGraph= require('./lib/viewer/woqlGraph')
-const axiosInstance= require('./lib/axiosInstance')
-const AccessControl= require('./lib/accessControl')
+const Vars = require('./lib/query/woqlDoc');
+const WOQLClient = require('./lib/woqlClient');
+const UTILS = require('./lib/utils');
+const View = require('./lib/viewer/woqlView');
+const WOQL = require('./lib/woql');
+const WOQLResult = require('./lib/viewer/woqlResult');
+const WOQLTable = require('./lib/viewer/woqlTable');
+const WOQLGraph = require('./lib/viewer/woqlGraph');
+const axiosInstance = require('./lib/axiosInstance');
+const AccessControl = require('./lib/accessControl');
 
 module.exports = {
   Vars,
