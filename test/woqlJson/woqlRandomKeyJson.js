0615a5a02c2a4bcea361fa67dce0012396c4ceca
Philippe Höij <philippe@hoij.net>
2025-11-29 07:55:25 +0100

    Adding the random id generation to WOQL JS

---
+++ test/woqlJson/woqlRandomKeyJson.js
@@ -0,0 +1,11 @@
+module.exports = {
+  '@type': 'RandomKey',
+  base: {
+    '@type': 'DataValue',
+    data: { '@type': 'xsd:string', '@value': 'Person/' },
+  },
+  uri: {
+    '@type': 'NodeValue',
+    variable: 'Person_ID',
+  },
+};
